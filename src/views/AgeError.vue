6b0779dacab4ca86f38351e1b356c3de88bd8a66
Ashen Wickramasinghe <ashenck@gmail.com>
2022-08-08 23:22:06 +0700

    Code alignments

--- src/views/AgeError.vue
+++ src/views/AgeError.vue
@@ -3,8 +3,7 @@
     <div class="card text-center">
       <h2>Ooops</h2>
       <p>
-        Your age is over our accepted limit.<br />We are sorry but we cannot
-        insure you now
+        Your age is over our accepted limit.<br />We are sorry but we cannot insure you now
       </p>
       <button class="button" @click="backHome()">Back</button>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="card text-center">
       <img src="https://img.freepik.com/premium-vector/family-standing-insurance-umbrella-together-shield-protection-parents-children-flat-vector-illustration-health-life-insurance-concept-banner-website-design-landing-web-page_179970-6637.jpg?w=2000" height="250px" />
       <h1>Hello There!</h1>
-      <p>Lets buy some insurance. It is going to take only a few steps</p>
+      <p>Lets buy some insurance. It is going to take only a few steps.</p>
       <button class="button button-blue" @click="startWizard()">Start</button>
     </div>
   </div>
